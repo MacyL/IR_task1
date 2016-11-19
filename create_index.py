29d9e1a232b6731ebe3ee29c0cd61cea88b5514f
MacyL <rorotira@gmail.com>
2016-11-19 11:30:29 +0100

    Update create_index.py

--- create_index.py
+++ create_index.py
@@ -5,13 +5,11 @@ import sys
 import csv
 from collections import defaultdict
 ##################################################################
-# Task : create index
-# command : python3 create_index_second.py document_input index_output
-# Idea : take lemma and document id, create a dictionary, add the key,value if the key does not exist
-# add the value if the key exist  
+# Task : create invert index. 
+# command : python3 create_index.py document_input index_output
 #################################################################
 print("Data read in...")
-
+# read in data, only need two columns, skip blank line
 myinput=list()
 with open(sys.argv[1],"r") as f:	
 	for l in f: 
@@ -19,12 +17,15 @@ with open(sys.argv[1],"r") as f:
 		if len(mycols)>1:
 			temp=tuple([mycols[2],int(mycols[5])])
 			myinput.append(temp)
+			
+# go through each element in input list, add the unique token as key, extend value list. 
 print("Create index...")
 myinput=set(myinput)
 myinput_dict=defaultdict(list)
 for v,t in myinput:
 	myinput_dict[v].append(t)
-		
+
+# formatting output. 		
 with open(sys.argv[2],"w") as myout:
 	writer = csv.writer(myout,delimiter='\t')
 	for key, value in myinput_dict.items():
